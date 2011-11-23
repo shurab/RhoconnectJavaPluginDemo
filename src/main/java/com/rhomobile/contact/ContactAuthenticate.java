c118b919fedb3d43460ae7126acdf250f9f4aee5
Alexander Babichev <alexb@rhomobile.com>
2011-11-23 10:28:56 -0800

    Cleaned project configuration.

--- src/main/java/com/rhomobile/contact/ContactAuthenticate.java
+++ src/main/java/com/rhomobile/contact/ContactAuthenticate.java
@@ -1,17 +1,16 @@
-package com.acme;
+package com.rhomobile.contact;
 
 import java.util.Map;
 import org.apache.log4j.Logger;
 import com.rhomobile.rhoconnect.Rhoconnect;
 
-public class AcmeAuthenticate implements Rhoconnect {
-	private static final Logger logger = Logger.getLogger(AcmeAuthenticate.class);	
+public class ContactAuthenticate implements Rhoconnect {
+	private static final Logger logger = Logger.getLogger(ContactAuthenticate.class);	
 	
 	@Override
 	public boolean authenticate(String login, String password, Map<String, Object> attributes) {
-		logger.info("AcmeAuthenticate#authenticate: implement your authentication code!");    		
+		logger.info("ContactAuthenticate#authenticate: implement your authentication code!");    		
         // TODO: your authentication code goes here ...
 		return true;
 	}
-
 }
